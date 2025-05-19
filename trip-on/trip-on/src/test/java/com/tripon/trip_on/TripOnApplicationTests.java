276b372bfa4f6ab53ae80a31d3bac63bad66af7f
tale1819 <kangminji116@gmail.com>
2025-05-19 14:37:19 +0900

    프론트

---
+++ trip-on/trip-on/src/test/java/com/tripon/trip_on/TripOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripon.trip_on;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
