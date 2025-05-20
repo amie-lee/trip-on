4deba081750c3c21082542e5370a382f99675e11
imHaiHye <imhaihye@gmail.com>
2025-05-20 14:30:45 +0900

    기본 프로필 설정

--- trip-on/src/main/java/com/tripon/trip_on/user/User.java
+++ trip-on/src/main/java/com/tripon/trip_on/user/User.java
@@ -1,6 +1,8 @@
 package com.tripon.trip_on.user;
 
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,7 +11,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-// @Table(name="users") //sql 테이블 이름 설정 (여기서 해도 되고 sql에서 해도 되는 듯)
+@Table(name="users") //sql 테이블 이름 설정 (여기서 해도 되고 sql에서 해도 되는 듯)
 public class User {
     @Id 
     @GeneratedValue(strategy=GenerationType.IDENTITY) // DB에서 알아서 값 자동 생성 (1,2,3...)
@@ -23,4 +25,14 @@ public class User {
 
     @Column(unique=true) // unique=true -> 한 DB에 하나만 있어야 함
     private String email;
+
+    @Column(name = "profile_image", nullable = false)
+    private String profileImage = "/images/tripon_profile_demo.png";
+
+    @Column(name = "reset_token", length=64)
+    private String resetToken;
+
+    @Column(name = "reset_token_expiry")
+    private LocalDateTime resetTokenExpiry;
+
 }
